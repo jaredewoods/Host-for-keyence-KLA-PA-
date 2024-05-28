cf525503987f61760811b1193f9dbe9b63301a2b
jaredewoods <155901160+jaredewoods@users.noreply.github.com>
2024-05-27 20:20:35 -0500

    fixed error receiving @

--- macro_status_window.py
+++ macro_status_window.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 
-class ProgramStatusWindow(tk.Tk):
+class MacroStatusWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Program Execution Status")
@@ -90,5 +90,5 @@ class ProgramStatusWindow(tk.Tk):
 
 
 if __name__ == "__main__":
-    app = ProgramStatusWindow()
+    app = MacroStatusWindow()
     app.mainloop()
